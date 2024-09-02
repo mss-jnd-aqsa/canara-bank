ef35f9d3b2cc408bd022d4b1e9e74189622df7be
Aneesuddin26 <ec2-user@ip-172-31-3-15.ap-south-1.compute.internal>
2024-09-02 08:05:48 +0000

    updated by anees

--- aness.java
+++ aness.java
@@ -1,1 +1,176 @@
 hello anees sir 
+hello this code is taken by anes i his laptop 
+byt git clone cmmand and this he is updating it 
+
+this is canara bank application code xpample 
+ The sun was harsh today. Through the lens, the scene was black and white, harsh shadows and stark highlights. Emma adjusted the aperture to be as small as it would go, cutting out much of the glare. The relative darkness restored faint traces of flavour to the frame, revealing the detail that the sunlight had tried to hide.
+
+A family stood huddled together, centred in a flat, if competent, composition. A man, with a bright smile and dark eyes, leaning close over his children, a pair that shared his expression. Behind them stretched a vast canyon, a great gaping wound in the land. It made for a wonderful memory.
+
+“Say cheese,” Emma said, the words mechanical.
+
+The vista was obscured for a split second as the shutter snapped closed, then open. Like a guillotine, it brought the moment to an abrupt end. She rose, elbows first, then one hand pushing her up and back onto her haunches. She shielded her eyes and studied the LED screen.
+
+On it was displayed, in miniature, a picture of the scene. Smiles, a hug, a family forever frozen in time. It wasn’t quite right.
+
+“How about another one?” she rasped.
+
+The family didn’t protest. They stood stock-still, all smiles. She lowered herself back into place, careless of the dust that would now cling to her loose shirt. Each step of the process was repeated, the family centred, the focus adjusted, the aperture tightened. As she regarded the scene, eye pressed to the viewfinder, she asked herself what was wrong with the first picture.
+
+She was in the right spot, she remembered where they had parked exactly, and the shot was framed perfectly. The weather was right, harsh sunlight browning the skin like crackling. They were all there, all in the positions they’d stood in. So what was it?
+
+Her eye was drawn to the smaller child. His expression was wrong, that was it. He hadn’t smiled, the heat had been too harsh on him. She withdrew from her camera, leaving it laying in the dust, and picked up Taylor by his head. She studied him as her spare hand rifled through her satchel.
+
+He was always pale, no matter how much sun he got, thin no matter how much he ate. He wasn’t sickly, per se, but he was frail. Maybe he’d have grown out of it.
+
+Her searching hand closed around the object of her desire: a thin sculpting pick. With it, she adjusted her son’s clay visage, pulling down the corners of his mouth and hooding his deep-set eyes.
+
+“There we go,” she muttered, replacing him in the scene, “that looks more like you.”
+
+It wasn’t as happy of a shot, but then, life isn’t always happy. She owed it to them, to represent them as they were.
+
+The second photo was better- not perfect, but better. It would never be quite right, but she had to try.
+
+The day passed and the sun crept through the sky. Emma shot, adjusted, and shot again, each time edging closer to a satisfactory recreation. It was only when the fire slunk over the edge of the horizon, and her camera protested of both a lack of space and low battery, that she finally relented.
+
+Good enough, she was forced to think, one of them will be good enough.
+
+She packed up her things, disassembling her camera and wrapping her miniatures with care. Her family had its own little box, one that she was in, too. But this picture had been taken without her, and so her facsimile had remained entombed.
+
+Standing was hard, her limbs imbued with terrific weight by her weariness. Tiny claws scratched at her throat as she heaved her satchel from the parched earth. The water in her canteen was warm.
+
+It wasn’t a long walk to her truck, but twilight had faded to dusk before she reached it. The back opened with a gentle wheeze, the softness of its effort mirroring the fatigue of its owner. It was difficult to find a place for her satchel, as the trunk was full to near the brim with albums. She selected a small stack, withdrawing them to make space, then closed the door on her memories.
+
+The albums she put on the floor in front of the passenger seat, then sat herself behind the wheel. Her camera still had a little battery, so she wheeled through her latest collection. They were arranged in chronological order, the first taken near noon, the last at nine thirty-two pm. All told, there were about two hundred. All of them had flaws, errors of framing, expression, composition, lighting. None were quite good enough.
+
+She was about halfway through the gallery when the screen shut off, leaving her staring at her own reflection. The face was not one she recognised, hollow and tired, with cracked lips and yellowed skin and eyes a little too wide. It was the face of a corpse.
+
+The photo would have to wait. She could charge the battery at her motel. Her cameras lived in the glove box, alongside registration papers, replacement lenses, and a dog-eared light romance novel Steve had liked to read to her as a joke. When she opened it to put the camera away, the final occupant plunked out onto the passenger seat.
+
+Emma gave the revolver a blank look. She’d bought it a month ago from a run-down surplus store just off the highway. It was a 44., “a lot of gun for a little lady,” as the clerk had put it. She didn’t care if he thought it was too much for her.
+
+She closed the glovebox and turned the ignition, leaving the weapon where it lay.
+
+There wasn’t much to see on the desert roads at night. Emma stared into the middle distance, mind empty. Her body knew the way back to the motel, letting her brain wander.
+
+“Long day, huh?” said the revolver.
+
+She didn’t respond.
+
+“It sure is beautiful out here. Peaceful. You picked a good place for a vacation.”
+
+The gun’s tone was familiar, friendly. The kind of voice you’d love to hear at the bar after work. She let it talk.
+
+“Y’know, I was manufactured not too far from here. Phoenix. Lovely city, very… hot.”
+
+Emma didn’t know if they even made guns in Phoenix, but she didn’t contradict it.
+
+“Got lots of family in the area. Granted, I got lotsa family all over the country. Heck, all over the world! Maybe we could swing by a range for a visit?”
+
+She hadn’t fired the handgun once. In fact, she’d only bought six bullets, just enough to load it. The clerk wouldn’t sell her just one.
+
+“… yeah, sorry, that was insensitive. Let’s just get back to the motel, you look like you need a good rest.”
+
+She did.
+
+“… an eight hour rest.”
+
+The motel was a motel. It had a big neon sign reading, ‘vacancy’, and a parking lot with a bunch of run-down vehicles. Her room key was dangling on the key-chain with her car key. The pair were the only ones she had left.
+
+The room was decent for the price, a single bed under a single yellow ceiling lamp and a single desk with a single chair. A shower and toilet waited in an adjoining room, functional but unexceptional. It was enough.
+
+She sank onto the bed, not bothering to remove more than her boots, uncaring of the dirt she scattered onto the sheets.
+
+Tomorrow, she thought, I’ll deal with it tomorrow.
+
+Sleep was a foreign country, one she wouldn’t visit if it weren’t necessary. Strange rituals played out here, theatre of memories and fantasies long forgotten. Her family was there, cavorting about in costumes they had never worn. Happiness had been so rare that she didn’t know what it was. All she knew was that it was better than what she usually felt.
+
+“Hey honey?” asked Steve, “do you know where the wedding photos are? I can’t find them.”
+
+“Oh, that’s my fault,” said her dream-self, “I haven’t got to those yet.”
+
+“Got to them?” he questioned, baffled.
+
+She was just as confused.
+
+“… I guess they might be upstairs.”
+
+“I’ll go check, thanks!”
+
+He kissed her on the cheek, and vanished.
+
+She was making dinner, it was time to eat.
+
+“Kids!” she called, “Steve!”
+
+There was no answer.
+
+“Guys?”
+
+Nothing. She was alone.
+
+Emma gasped as she awoke, heart pounding, lungs tight. The room was burning, orange light piercing, dancing, the flames roaring as they burned through. The air was choked with smoke. She couldn’t breathe, couldn’t move, could only wait for the fire to devour her. But worst of all, she heard children screaming.
+
+The bedcovers flew off of her as she sat bolt upright. She gasped for air, eyes twitching wildly from spot to spot. There was no fire. The orange was streetlamps, the roaring a motorbike in the distance. The smoke was from a cigarette, snaking through her cracked-open window, and a siren sounded for screams.
+
+Panic faded into nausea. It was too hot, even with the window open- she needed fresh air. So she got up and escaped into the night. The open half-light of the parking lot was cooler, the desert’s night sky welcoming the day’s heat. There were few stars here, outcompeted by the streetlamps and city skyline.
+
+Emma patrolled the parking lot, walking to dissipate the energy of her memory-dream. But the scene held nothing in it that could distract from her nightmare. Fire, and screaming. Worse than that, silence.
+
+Rain fell, teardrops watering the concrete in vain. As terrible as the dreams were, waking up was worse. Fear was better than nothing. Her marching feet brought her to a stop next to her truck. She flattened her hands against the window, shaky breaths interspersed with sobs. The fog of her breath couldn’t quite obscure the view of the passenger seat.
+
+Her fingers found the door handle, then curled around the grip of her revolver. She looked at it, tracing the lines of the metal with her eyes, feeling the weight of it in her hand. It was heavy, for something so small.
+
+Slowly it turned, and lifted, pressing its barrel into her forehead. Silence reigned, as if the world was holding its breath. To any passers-by, it would look as if she were praying.
+
+Why don’t you just do it? She asked herself. What’s the point of this?
+
+She didn’t have an answer. Ever since the night her home burned down, she’d been a ghost. She might as well have died in that fire. At first, she’d promised to live the life her family couldn’t have. Weeping, broken, at their graveside, she’d sworn that she’d go on. But no amount of promises could pull the metal away from her cranium. What else was there to live for?
+
+She’d tried to relive the past, after that. All those albums, filled with pictures of miniatures, remaking moments lost to ash. As if, somehow, these ersatz photographs could stop her memory from fading. But every day the images were less distinct. Now, she couldn’t pick a photo from her digital gallery, because she didn’t know if any of them were correct.
+
+Her tears dried, sorrow replaced by numb machination. Logic attempted to dissuade her, to tell her that she was being irrational, that if she was alive, that meant there was hope. Her heart didn’t agree. Hope was in the end, because maybe they were waiting for her, somewhere else.
+
+She’d just about convinced herself when a noise drew her attention. She looked over.
+
+The nearest streetlamp drew a spotlight on the paved sidewalk, perfectly illuminating a pile of garbage. Standing on said garbage, one foreleg extended and the other curled, peering curiously at the peak of the pile, was a cat. A natty, skinny cat, with a halo of curly hair. Emma watched as it climbed, placing each paw cautiously, sniffing the air. Eventually, it reached its target, a torn black bag from which something vaguely edible hung.
+
+The cat pulled its prize out of the bag; a fish tail. It examined the meal, sweeping nostrils across it, before nibbling on the meatier end.
+
+Emma felt a mirror had been held up. It was not a flattering image. Indeed, it was the last straw. Her finger squeezed the trigger.
+
+Click.
+
+Empty. She’d never loaded the gun. It fell to her side, held lightly in a limp hand. The cat glanced over at the noise, giving her an imperious look. The sight of this animal, dirty and thin, eating garbage and still so superior, was motivating.
+
+She opened her car door, then the glovebox, where the bullets lived. She loaded her weapon, then turned to face the cat. She raised her hands, holding the weapon steady in both, one eye closed to better aim at the animal. It gave her another look of seeming disdain. She took the shot.
+
+Click-shzzz.
+
+She took the photograph from her Polaroid’s printer, shaking it to help the picture develop faster. After a moment, an image painted itself into view: the cat, sitting on its garbage throne, its stinky dinner between its paws. The feline’s pride, even in the midst of squalor, brought the faintest of smiles to her lips. It was the first real picture she’d taken since that night.
+
+In the inky depths of despair, a spark lit. One that refused to die. The tiniest hint of joy. She’d forgotten, in the rush to recreate her family’s photo albums, why there had been so many to begin with.
+
+She looked up, gratitude in her eyes, but the cat, and their prize, were gone. Clearly, they didn’t have time for a mangy human with lifeless eyes. But that didn’t matter, because for as much as Emma had been a spectre, she saw her way back to life. A way back, maybe not to happiness, not for a long time, but perhaps to movement. A way to continue on alone.
+
+The past was dead, and it was time to bury it.
+
+She took every album of fabricated souvenirs, every single miniature figure of her lost family, all of her modelling tools, her undeveloped film and the memory card from her digital camera, and piled them on the altar of trash. It was an offering, of sorts, appeasement to the dead. She ran to the front office and paid for her room, apologising for the mess, then ran back to her truck.
+
+She felt giddy. Her trunk was empty. On the seat next to her lay her single remaining album, completely free of any pictures. She opened it up, and slotted the photo of the cat into the first pocket. It really was a funny one. She lay it back down, and picked up her revolver. It still wasn’t loaded.
+
+“I knew you wanted to live.” It said, as she carried it over to the drain.
+
+“Hush.” She replied.
+
+“Cheriooooooooo…” was the last she heard of it, as it disappeared into the sewers.
+
+It was irresponsible, she knew, but she didn’t care. Her life was hers again, even if she still had a long way to go.
+
+Emma got into her truck, turned the ignition, and drove to the exit of the parking lot. She had no idea where she was going, but she knew that she wouldn’t be back here. Before she headed off into the night, she checked her mirror, catching a glimpse of the pile of albums. Maybe it was a trick of the light, but she thought she saw three spirits wavering there. They were smiling, and waving.
+
+I love you, she mouthed, pressing a kiss into her fingers, then up into the mirror.
+
+They faded away, and she wiped the tears from her cheeks. Changing to first, she was off, driving into the light of dawn.
+
+It was time to live again. She owed nothing to ghosts.
